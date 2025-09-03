eae01f18aee4c8a7bccd12414cc1084b09630462
Diego Tonguino <diegotonguino3@gmail.com>
2025-09-03 12:43:51 -0500

    Agregado Enterprise Business Rules

--- NorthWind.Sales.Entities/obj/Debug/net8.0/NorthWind.Sales.Entities.AssemblyInfo.cs
+++ NorthWind.Sales.Entities/obj/Debug/net8.0/NorthWind.Sales.Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NorthWind.Sales.Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a7838350a890d8c5c91c33caaf4288175af3a6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("NorthWind.Sales.Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NorthWind.Sales.Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
