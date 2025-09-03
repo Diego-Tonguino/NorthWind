4a7838350a890d8c5c91c33caaf4288175af3a6b
Diego Tonguino <diegotonguino3@gmail.com>
2025-09-03 12:37:29 -0500

    Proyecto Creado

---
+++ NorthWind.Sales.Entities/Dtos/CreateOrder/CreateOrderDetailDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Sales.Entities.Dtos.CreateOrder;
+
+internal class CreateOrderDetailDto
+{
+}
